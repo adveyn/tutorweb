b63a85243ed8037c7dc1d138cfaf58d680f6e9a4
adveyn <52429350+adveyn@users.noreply.github.com>
2023-06-03 19:36:50 -0400

    Update urls.py

--- ICS4U/ICS4U/ICS4U/urls.py
+++ ICS4U/ICS4U/ICS4U/urls.py
@@ -1,10 +1,10 @@
-#Navbar: https://www.youtube.com/watch?v=qNifU_aQRio
-#Django Setup: https://www.youtube.com/watch?v=LQTMqGns7Co
-#HTML classes: https://www.w3schools.com/html/html_classes.asp
-#https://docs.djangoproject.com/en/4.1/topics/class-based-views/
-#https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
-#https://stackoverflow.com/questions/25187903/what-do-curly-braces-around-javascript-variable-name-mean
+'''
+Author: Advey Nandan
+Last Modified: June 2, 2023
+DJANGO VERSION 4.1.5
 
+Urls
+'''
 
 from django.contrib import admin
 from django.urls import path, include
